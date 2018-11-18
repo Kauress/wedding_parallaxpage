346653fd52126b5674840bb2945b82bf512b9e4d
kauress8 <kauress8@gmail.com>
2018-11-17 22:48:08 -0700

    initial commit

---
+++ js/just.js
@@ -0,0 +1,46 @@
+   <div class="container">
+     <div class='row'>
+        <div class="col-lg-12" data-start="opacity:0;" data-100-start="opacity:1;"> <h1> Our Story (scroll down)</h1> </div>
+     </div>
+</div>
+
+
+ 
+       <div class="container" id="third-section" data-top="opacity:0; top:0px;" data--150-top="opacity:1; top:-1000px;">
+        <div class="row">
+            <div class="col-lg-12">
+               <h3 data-top="opacity:0; " data--300-top="opacity:1" > 16th September 2015: First Date</h3>
+             <img data-top="opacity:0; transform:scale(.5)" data--500-top="opacity:1;transform:scale(1)" src="img/wedding_32a.png" class="img-responsive center-block">
+
+             <h3 data-top="opacity:0" data--300-top="opacity:1" > 16th September 2015: First Date</h3>
+             <img data-top="opacity:0;transform:scale(.5)" data--500-top="opacity:1;transform:scale(1)" src="img/wedding_31a.png" class="img-responsive center-block">
+  
+             <h3 data-top="opacity:0" data--300-top="opacity:1" > 16th September 2015: First Date</h3>
+             <img data-top="opacity:0;transform:scale(.5)" data--500-top="opacity:1;transform:scale(1)" src="img/wedding_39a.png" class="img-responsive center-block">
+
+         <h3  data-top="opacity:0" data--300-top="opacity:1" > 16th September 2015: First Date</h3>
+             <img data-top="opacity:0;transform:scale(.5)" data--500-top="opacity:1;transform:scale(1)" src="img/wedding_32a.png" class="img-responsive center-block" id="last">
+
+
+ <h3 data-top="opacity:0" data--50-top="opacity:1" > Proposal</h3>
+             <img  data-top="opacity:0; top:0px; "  data--490-top="opacity:1; top:0px" data--1080-top="opacity:1; top:570px;"   src="img/couple.jpg" class="img-responsive center-block">
+              <div> <!-- col-md -12 -->
+           </div>  <!-- row -->
+       </div>  <!-- container -->
+
+
+
+<!-- Skrollr -->
+    <script type="text/javascript">
+    var s = skrollr.init({
+        edgeStrategy: 'set',
+        forceHeight: false,
+        smoothScrolling: false,
+        easing: {
+            WTF: Math.random,
+            inverted: function(p) {
+                return 1-p;
+            }
+        }
+    });
+    </script>
